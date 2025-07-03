fffa55244195dc2b9bfb8282764de44e6495c241
igupz <138035194+igupz@users.noreply.github.com>
2025-07-03 19:26:35 -0300

    Delete frontend directory

--- frontend/src/pages/Home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Campeonatos Inscritos</h1>
-    <div class="bg-white shadow rounded p-4">Nenhum campeonato encontrado.</div>
-  </div>
-</template>
--- frontend/src/pages/Rankings.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Classificação</h1>
-    <div class="bg-white shadow rounded p-4">Ranking dos times/jogadores em breve.</div>
-  </div>
-</template>
